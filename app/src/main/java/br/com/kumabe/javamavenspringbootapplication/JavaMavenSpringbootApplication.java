e19adf69ac8f2abab379bce6fd9380b26583d90d
Rodrigo Kumabe <rodrigo.kumabe@gmail.com>
2023-03-05 13:51:18 -0300

    first commit

---
+++ app/src/main/java/br/com/kumabe/javamavenspringbootapplication/JavaMavenSpringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.kumabe.javamavenspringbootapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMavenSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaMavenSpringbootApplication.class, args);
+	}
+
+}
---
+++ app/src/test/java/br/com/kumabe/javamavenspringbootapplication/JavaMavenSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kumabe.javamavenspringbootapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMavenSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
